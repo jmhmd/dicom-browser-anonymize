76a9e419703f1aa6c42cc73d84bc01c5aa5f8f94
jmhmd <jhostetter@gmail.com>
2021-11-29 13:03:43 -0500

    Fix background tab throttling

--- src/aTick.ts
+++ src/aTick.ts
@@ -1,9 +1,12 @@
 /**
  * Await a timeout of 0 milliseconds to allow Vue to update DOM when doing resource intensive
- * processing loops
+ * processing loops. Skip if tab is not visible to prevent throttling from stopping all processing.
  * @return {Promise<void>}
  */
 export default async function aTick() {
+  if (document.hidden) {
+    return;
+  }
   return new Promise<void>((resolve) => {
     setTimeout(() => resolve(), 0);
   });
